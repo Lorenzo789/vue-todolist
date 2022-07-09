181131e98f8782687ad4b579cf10c3ca0226d72e
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-09 13:50:31 +0200

    bonus 2

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo vie
 
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa
  */
 
 
@@ -42,13 +46,17 @@ const app = new Vue (
             },
 
             addNewToDo: function(toDoText) {
-                const newMessage = {
-                    text: toDoText,
-                    done: false
+                if (toDoText != '') {
+                    const newMessage = {
+                        text: toDoText,
+                        done: false
+                    }
+                    this.todos.push(newMessage);
+    
+                    this.newToDo = '';     
+                } else {
+                    alert('Stai Inserendo un Todo Vuoto');
                 }
-                this.todos.push(newMessage);
-
-                this.newToDo = '';
             }
         }
     }
